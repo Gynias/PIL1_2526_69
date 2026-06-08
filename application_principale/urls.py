7d78d9354cb746207df21e07986eee8843b23117
Evan <evanazankpan@gmail.com>
2026-06-08 21:55:28 +0100

    Update urls.py

--- application_principale/urls.py
+++ application_principale/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('profil/', views.mon_profil, name='mon_profil'),
+    path('profil/modifier/', views.modifier_profil, name='modifier_profil'),
+    path('profil/<int:user_id>/', views.profil_public, name='profil_public'),
+]
