049a10d90dbef7d96af8f11bacdd2f148a3fba43
Evan <evanazankpan@gmail.com>
2026-06-10 19:18:49 +0100

    Update urls.py

--- application_principale/urls.py
+++ application_principale/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('profil/', views.mon_profil, name='mon_profil'),
     path('profil/modifier/', views.modifier_profil, name='modifier_profil'),
     path('profil/<int:user_id>/', views.profil_public, name='profil_public'),
+    path('competence/ajouter/', ajouter_competence, name='ajouter_competence'),
+    path('competence/supprimer/<int:competence_id>/', supprimer_competence, name='supprimer_competence'),
 ]
